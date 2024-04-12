6c73fed5712998d61d3453572c95dff135db0b35
Daniel Calderon <danycalvir@gmail.com>
2024-04-11 21:27:09 -0700

    deploy

---
+++ app/layout.tsx
@@ -0,0 +1,57 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { ModalProvider } from "@/components/providers/modals-provider";
+import { EdgeStoreProvider } from "../lib/edgestore";
+
+import "./globals.css";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Dotion",
+  description: "The connected workspaces",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="dotion-theme-2"
+            >
+              <Toaster />
+              <ModalProvider />
+              {children}
+            </ThemeProvider>
+          </EdgeStoreProvider>
+        </ConvexClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/providers/modals-provider.tsx
@@ -0,0 +1,22 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import { SettingModal } from '../modals/setting-modal'
+import { CoverImageModal } from '../modals/cover-image-modal';
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+  
+  return (
+    <>
+      <SettingModal/>
+      <CoverImageModal/>
+    </>
+  )
+}
